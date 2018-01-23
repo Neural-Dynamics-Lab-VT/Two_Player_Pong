52ad452773eea6129a296dceca5a18855dfd5438
naresh1318 <rnaresh.n@gmail.com>
2018-01-23 17:03:39 -0500

    trouble using signal stream

--- pong_example.py
+++ pong_example.py
@@ -1,22 +1,31 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty,\
-    ObjectProperty
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
 from kivy.vector import Vector
 from kivy.clock import Clock
 from kivy.core.window import Window
 from pylsl import StreamInlet, resolve_stream
 
 
-print("looking for an EEG stream...")
-streams = resolve_stream('type', 'EEG')
+def get_lsl_signal_stream():
+    """
+    Uses pylsl to receive the EEG stream from the cap. Must ideally have two streams.
+    """
+    print("looking for an EEG stream...")
+    streams = resolve_stream('type', 'EEG')
+    print("Received no of streams: {}".format(len(streams)))
 
-# create a new inlet to read from the stream
-inlet_1 = StreamInlet(streams[0])
-inlet_2 = StreamInlet(streams[1])
+    # create a new inlet to read from the stream
+    inlet_1 = StreamInlet(streams[0])
+    inlet_2 = StreamInlet(streams[1])
+
+    return inlet_1, inlet_2
 
 
 class PongPaddle(Widget):
+    """
+    Widget used to specify the ball bouncing off of the paddle.
+    """
     score = NumericProperty(0)
 
     def bounce_ball(self, ball):
@@ -29,6 +38,9 @@ class PongPaddle(Widget):
 
 
 class PongBall(Widget):
+    """
+    Widget the describes the ball and its velocity.
+    """
     velocity_x = NumericProperty(0)
     velocity_y = NumericProperty(0)
     velocity = ReferenceListProperty(velocity_x, velocity_y)
@@ -38,6 +50,10 @@ class PongBall(Widget):
 
 
 class PongGame(Widget):
+    """
+    This is the real deal. Contains events handlers for keyboard inputs if pressed, updates the
+    ball velocity on collision and also adds touchscreen controls (kinda useless for now).
+    """
     ball = ObjectProperty(None)
     player1 = ObjectProperty(None)
     player2 = ObjectProperty(None)
@@ -46,17 +62,17 @@ class PongGame(Widget):
         super(PongGame, self).__init__(**kwargs)
         self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
         self._keyboard.bind(on_key_down=self._on_keyboard_down)
-        self.old_key = 0
         self.speed = 25
-        self.sample = None
-        self.timestamp = None
+        self.beta_threshold = 25
+
+        # TODO: Add paddle momentum
+        # TODO: Paddle forced to move to a fi
 
     def _keyboard_closed(self):
         self._keyboard.unbind(on_key_down=self._on_keyboard_down)
         self._keyboard = None
 
     def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
-
         if keycode[1] == 'w':
             print("Up")
             self.player1.center_y += self.speed
@@ -76,8 +92,8 @@ class PongGame(Widget):
     def update(self, dt):
         self.ball.move()
 
-        self.sample_1, self.timestamp_1 = inlet_1.pull_sample()
-        self.sample_2, self.timestamp_2 = inlet_2.pull_sample()
+        sample_1, _ = inlet_1.pull_sample()
+        sample_2, _ = inlet_2.pull_sample()
 
         # bounce of paddles
         self.player1.bounce_ball(self.ball)
@@ -95,16 +111,16 @@ class PongGame(Widget):
             self.player1.score += 1
             self.serve_ball(vel=(-4, 0))
 
-        if self.sample_1[0] > 0.5:
+        if sample_1[0] > self.beta_threshold:
             self.player1.center_y += self.speed
 
-        if self.sample_1[0] <= 0.5:
+        if sample_1[0] <= self.beta_threshold:
             self.player1.center_y -= self.speed
 
-        if self.sample_2[0] > 0.5:
+        if sample_2[0] > self.beta_threshold:
             self.player2.center_y += self.speed
 
-        if self.sample_2[0] <= 0.5:
+        if sample_2[0] <= self.beta_threshold:
             self.player2.center_y -= self.speed
 
     def on_touch_move(self, touch):
@@ -115,12 +131,17 @@ class PongGame(Widget):
 
 
 class PongApp(App):
+    """
+    A class to start the game.
+    """
     def build(self):
         game = PongGame()
         game.serve_ball()
-        Clock.schedule_interval(game.update, 1.0 / 120.0)
+        Clock.schedule_interval(game.update, 1.0 / 300.0)
+        print("The game's running")
         return game
 
 
 if __name__ == '__main__':
+    inlet_1, inlet_2 = get_lsl_signal_stream()
     PongApp().run()
--- sample_data_acquisition.py
+++ sample_data_acquisition.py
@@ -7,10 +7,12 @@ print("looking for an EEG stream...")
 streams = resolve_stream('type', 'EEG')
 
 # create a new inlet to read from the stream
-inlet = StreamInlet(streams[0])
+inlet = StreamInlet(streams[1])
+
+print("No of streams: {}".format(len(streams)))
 
 while True:
     # get a new sample (you can also omit the timestamp part if you're not
     # interested in it)
     sample, timestamp = inlet.pull_sample()
-    print(timestamp, sample)
+    print(sample)
